a7d64bae1751c974f010eabdda0dec0090a0cdb5
Bill Zorn <bill.zorn@gmail.com>
2017-07-19 11:00:26 -0700

    add x64 clang/llvm to example

--- example_undef.c
+++ example_undef.c
@@ -71,3 +71,99 @@ Disassembly of section .text:
    0:   31 c0                   xor    %eax,%eax
    2:   c3                      retq   
 */
+
+
+
+// 2 interpretations by clang
+
+// O0 is similar to GCC, but not as efficiently compiled.
+
+// O1 and above have already identified the optimization
+// and taken advantage of it. Notice the "nsw" attribute
+// in the O0 llvm code.
+
+/*
+; ModuleID = 'example_undef.bc'
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+; Function Attrs: nounwind uwtable
+define i32 @test_intmax(i32 %x) #0 {
+  %1 = alloca i32, align 4
+  %2 = alloca i32, align 4
+  store i32 %x, i32* %2, align 4
+  %3 = load i32, i32* %2, align 4
+  %4 = add nsw i32 %3, 1
+  %5 = load i32, i32* %2, align 4
+  %6 = icmp sgt i32 %4, %5
+  br i1 %6, label %7, label %8
+
+; <label>:7                                       ; preds = %0
+  store i32 0, i32* %1, align 4
+  br label %9
+
+; <label>:8                                       ; preds = %0
+  store i32 1, i32* %1, align 4
+  br label %9
+
+; <label>:9                                       ; preds = %8, %7
+  %10 = load i32, i32* %1, align 4
+  ret i32 %10
+}
+
+attributes #0 = { nounwind uwtable "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-feat
+ures"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.ident = !{!0}
+
+!0 = !{!"clang version 3.8.0-2ubuntu4 (tags/RELEASE_380/final)"}
+
+
+example_undef.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <test_intmax>:
+   0:   55                      push   %rbp
+   1:   48 89 e5                mov    %rsp,%rbp
+   4:   89 7d f8                mov    %edi,-0x8(%rbp)
+   7:   8b 7d f8                mov    -0x8(%rbp),%edi
+   a:   83 c7 01                add    $0x1,%edi
+   d:   3b 7d f8                cmp    -0x8(%rbp),%edi
+  10:   0f 8e 0c 00 00 00       jle    22 <test_intmax+0x22>
+  16:   c7 45 fc 00 00 00 00    movl   $0x0,-0x4(%rbp)
+  1d:   e9 07 00 00 00          jmpq   29 <test_intmax+0x29>
+  22:   c7 45 fc 01 00 00 00    movl   $0x1,-0x4(%rbp)
+  29:   8b 45 fc                mov    -0x4(%rbp),%eax
+  2c:   5d                      pop    %rbp
+  2d:   c3                      retq   
+*/
+
+/*
+; ModuleID = 'example_undef.bc'
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+; Function Attrs: norecurse nounwind readnone uwtable
+define i32 @test_intmax(i32 %x) #0 {
+  ret i32 0
+}
+
+attributes #0 = { norecurse nounwind readnone uwtable "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="false" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,
++mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.ident = !{!0}
+
+!0 = !{!"clang version 3.8.0-2ubuntu4 (tags/RELEASE_380/final)"}
+
+
+example_undef.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <test_intmax>:
+   0:   31 c0                   xor    %eax,%eax
+   2:   c3                      retq   
+*/
